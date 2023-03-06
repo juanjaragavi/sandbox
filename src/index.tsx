4c098eab44bc4f4012573fffd41b80d1e9c46980
Juan Jaramillo <125833402+juanjaragavi@users.noreply.github.com>
2023-03-06 13:55:48 -0500

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { render } from "react-dom";
+import { Example } from "./Example";
+import "./styles.css";
+
+const App = () => <Example />;
+
+render(<App />, document.getElementById("root"));
